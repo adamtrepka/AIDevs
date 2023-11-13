328d95e23cbfc12cb5b63f9b6724493b258a8236
Adam Trepka <adamtrepk@gmail.com>
2023-11-13 23:14:46 +0100

    feat: add time span semaphore

---
+++ src/Shared/AIDevs.Shared.Infrastructure/TimeSpanSemaphore.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Diagnostics;
+
+namespace AIDevs.Shared.Infrastructure
+{
+    /// <summary>
+    /// Allows a limited number of acquisitions during a timespan
+    /// </summary>
+    public sealed class TimeSpanSemaphore : IDisposable
+    {
+        private readonly SemaphoreSlim _pool;
+
+        // the time span for the max number of callers
+        private readonly TimeSpan _resetSpan;
+
+        // keep track of the release times
+        private readonly Queue<DateTime> _releaseTimes;
+
+        // protect release time queue
+        private readonly object _queueLock = new object();
+
+        public TimeSpanSemaphore(int maxCount, TimeSpan resetSpan)
+        {
+            _pool = new SemaphoreSlim(maxCount, maxCount);
+            _resetSpan = resetSpan;
+
+            // initialize queue with old timestamps
+            _releaseTimes = new Queue<DateTime>(maxCount);
+            for (var i = 0; i < maxCount; i++)
+            {
+                _releaseTimes.Enqueue(DateTime.MinValue);
+            }
+        }
+
+        /// <summary>
+        /// Blocks the current thread until it can enter the semaphore, while observing a CancellationToken
+        /// </summary>
+        private async Task Wait()
+        {
+            // will throw if token is cancelled
+            _pool.Wait();
+
+            // get the oldest release from the queue
+            DateTime oldestRelease;
+            lock (_queueLock)
+            {
+                oldestRelease = _releaseTimes.Dequeue();
+            }
+
+            // sleep until the time since the previous release equals the reset period
+            var now = DateTime.UtcNow;
+            var windowReset = oldestRelease.Add(_resetSpan);
+            if (windowReset > now)
+            {
+                var sleepMilliseconds = Math.Max(
+                    (int)(windowReset.Subtract(now).Ticks / TimeSpan.TicksPerMillisecond),
+                    1); // sleep at least 1ms to be sure next window has started
+                Debug.WriteLine("Waiting {0} ms for TimeSpanSemaphore limit to reset.", sleepMilliseconds);
+
+                await Task.Delay(sleepMilliseconds);
+            }
+        }
+
+        /// <summary>
+        /// Exits the semaphore
+        /// </summary>
+        private void Release()
+        {
+            lock (_queueLock)
+            {
+                _releaseTimes.Enqueue(DateTime.UtcNow);
+            }
+            _pool.Release();
+        }
+
+        /// <summary>
+        /// Runs an action after entering the semaphore (if the CancellationToken is not canceled)
+        /// </summary>
+        public async Task Run(Func<Task> action)
+        {
+            // will throw if token is cancelled, but will auto-release lock
+            await Wait();
+
+            try
+            {
+                await action();
+            }
+            finally
+            {
+                Release();
+            }
+        }
+
+        /// <summary>
+        /// Runs an action after entering the semaphore (if the CancellationToken is not canceled)
+        /// </summary>
+        public async Task<T> Run<T>(Func<Task<T>> action)
+        {
+            await Wait();
+
+            try
+            {
+                return await action();
+            }
+            finally
+            {
+                Release();
+            }
+        }
+
+        /// <summary>
+        /// Releases all resources used by the current instance
+        /// </summary>
+        public void Dispose()
+        {
+            _pool.Dispose();
+        }
+    }
+}
---
+++ src/Tests/AIDevs.Tests.Unit/Infrastructure/TimespanSemaphoreTests.cs
@@ -0,0 +1,57 @@
+﻿using AIDevs.Shared.Infrastructure.Embedding;
+using AIDevs.Tests.Unit.Exercises.UnknowNews;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIDevs.Tests.Unit.Infrastructure
+{
+    public class TimespanSemaphoreTests
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public TimespanSemaphoreTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public async Task Should_Generate_Embeddings()
+        {
+            // Arrange
+            var textEmbeddings = new TextEmbeddings(new HttpClient()
+            {
+                BaseAddress = new Uri("http://127.0.0.1:8080/"),
+            });
+
+            var httpClient = new HttpClient();
+            var httpResponse = await httpClient.GetAsync("https://unknow.news/archiwum.json");
+            var archive = await httpResponse.Content.ReadFromJsonAsync<List<Article>>();
+
+            var embedings = new List<Embedding<Article>>(archive.Count);
+
+            // Act
+
+            using var semaphore = new TimeSpanSemaphore(100, TimeSpan.FromSeconds(10));
+
+            Parallel.ForEach(archive, async article =>
+            {
+                var embedding = await semaphore.Run(() => textEmbeddings.GetEmbeddingsAsync(new(article.Info, true)));
+
+                embedings.Add(Embedding<Article>.Create(article, embedding));
+                Debug.WriteLine($"Embeddings count: {embedings.Count}/{archive.Count}");
+            });
+
+            var questionEmbedding = await textEmbeddings.GetEmbeddingsAsync(new("Raspberry",true));
+
+            var answer = embedings
+                .OrderByDescending(embedding => Extensions.CosineSimilarity(questionEmbedding, embedding.Vector))
+                .Take(4);
+        }
+    }
+}
